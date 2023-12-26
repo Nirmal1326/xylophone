9b3386d5f72e31a9d629accdb2c8f89bce31b3c9
nirmal1326 <nirmal06112002@gmail.com>
2023-12-26 21:05:01 +0530

    Initial commit

---
+++ lib/sample.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
